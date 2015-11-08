f8ba5345403ca546f588f3c4ab9ebffaddae5636
haley <haley720724@gmail.com>
2015-11-08 16:51:53 +0800

    19 使用 EFGeneratePartialClass.tt 產生 Entity Framework 實體模型的 Partial Class

---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,30 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(1.0, 99.9)]
+        [DisplayFormat(DataFormatString = "{0:C1}")]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        public Nullable<bool> Active { get; set; }
+        [Required]
+        public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
