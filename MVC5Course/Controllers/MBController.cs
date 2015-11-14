c7e33b985f13e824f573f4f5ea0e09a166889da1
haley <haley720724@gmail.com>
2015-11-14 16:57:17 +0800

    30 練習 ViewData.Model, ViewData["xxx"], ViewBag.xxx, TempData["xxx"] 的用法

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            var data = new NewProductVM()
+            {
+                Price = 100,
+                ProductName = "T-Shirt"
+            };
+
+            ViewData["MyTitle"] = "will";
+            ViewBag.MyTitle = "will 2";
+
+            var db = new FabricsEntities();
+            db.Configuration.LazyLoadingEnabled = false;
+            ViewBag.Products = db.Product.Take(5).ToList();
+
+            TempData["Msg"] = "TEST!";
+
+            ViewData.Model = data;
+            return View();
+        }
+    }
+}
