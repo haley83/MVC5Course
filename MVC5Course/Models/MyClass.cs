8417026aca984b02f6107aa1cba3438e2b92dd1f
haley <haley720724@gmail.com>
2015-11-21 16:06:14 +0800

    46 練習利用 DisplayTemplates 與 EditorTemplates 重構 View 頁面架構

---
+++ MVC5Course/Models/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+
+    enum sex
+	{
+	        male,
+        female
+	}
+	
+
+    public class MyClass
+    {
+        public int MyProperty { get; set; }
+
+
+        public MyClass()
+        {
+            sex e = sex.male;
+            switch (e)
+            {
+                case sex.male:
+                    break;
+                case sex.female:
+                    break;
+                default:
+                    break;
+            }
+
+
+            //var o = new ApplicationUser();
+
+
+        }
+    }
+}
