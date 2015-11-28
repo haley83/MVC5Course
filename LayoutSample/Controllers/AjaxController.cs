9eb33112c2143cafbbc9ab15340aefc73f2848e0
haley <haley720724@gmail.com>
2015-11-28 09:38:45 +0800

    49 練習使用 @Ajax.ActionLink 完成基本 Ajax 操作 ( 記得安裝必要的 NuGet 套件並將 JS 載入網頁中 )

---
+++ LayoutSample/Controllers/AjaxController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutSample.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetTime()
+        {
+            return Content(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffff"));
+        }
+    }
+}
